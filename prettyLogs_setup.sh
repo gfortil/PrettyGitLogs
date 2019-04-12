30f556e857deee4c889cc87edd3acbbdae5101ce
Godson Fortil <Godson.Fortil@Lexisnexisrisk.com>
2019-04-12 03:27:38 -0400

    Add Date::PeriodParser dependency
    Signed-off-by: Godson Fortil <Godson.Fortil@Lexisnexisrisk.com>

--- prettyLogs_setup.sh
+++ prettyLogs_setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 sudo cpan App::cpanminus
-sudo cpanm DateTime --force
+sudo cpanm DateTime 
 sudo cpanm App::Gitc::Its::Jira
 sudo cpanm XMLRPC::Lite
 sudo cpanm Term::ReadKey
 sudo cpanm Config::Simple
 sudo cpanm JIRA::REST
+sudo cpanm Date::PeriodParser --force
+
 
 echo "Done"
 echo "Now Run PrettyLogs.pl"
