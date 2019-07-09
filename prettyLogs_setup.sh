cd88361d0c2abcb3062c6fcad3cc5f57d31044b0
Godson Fortil <Godson.Fortil@Lexisnexisrisk.com>
2019-07-09 11:47:29 -0400

    Force dependencies to be installed
    Signed-off-by: Godson Fortil <Godson.Fortil@Lexisnexisrisk.com>

--- prettyLogs_setup.sh
+++ prettyLogs_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo cpan App::cpanminus
-sudo cpanm DateTime 
-sudo cpanm App::Gitc::Its::Jira
-sudo cpanm XMLRPC::Lite
-sudo cpanm Term::ReadKey
-sudo cpanm Config::Simple
-sudo cpanm JIRA::REST
+sudo cpan App::cpanminus --force
+sudo cpanm DateTime --force
+sudo cpanm App::Gitc::Its::Jira --force
+sudo cpanm XMLRPC::Lite --force
+sudo cpanm Term::ReadKey --force
+sudo cpanm Config::Simple --force
+sudo cpanm JIRA::REST --force
 sudo cpanm Date::PeriodParser --force
-sudo cpanm Config::Identity
+sudo cpanm Config::Identity --force
 
 echo "Done"
 echo "Now Run PrettyLogs.pl"
